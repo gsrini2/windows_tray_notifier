8a80c6080f46474f6f303176f0b7f87e0fcf5035
gsrini2 <gsrini2@yahoo.com>
2017-09-28 12:57:40 +0530

    Create fnotify.py

---
+++ fnotify.py
@@ -0,0 +1,61 @@
+'''
+This python script generates a tray notification in windows OS.
+The custom message to be shown can be passed as an argument.
+This module can be imported as such and used in other files to 
+display the information needed.
+
+NOTE: This source code is not mine and was taken from online sources.
+'''
+from win32api import *
+from win32gui import *
+import win32con
+import sys, os
+import struct
+import time
+
+class WindowsBalloonTip:
+    def __init__(self, title, msg):
+        message_map = {
+                win32con.WM_DESTROY: self.OnDestroy,
+        }
+        # Register the Window class.
+        wc = WNDCLASS()
+        hinst = wc.hInstance = GetModuleHandle(None)
+        wc.lpszClassName = "PythonTaskbar"
+        wc.lpfnWndProc = message_map # could also specify a wndproc.
+        classAtom = RegisterClass(wc)
+        # Create the Window.
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        self.hwnd = CreateWindow( classAtom, "Taskbar", style, \
+                0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
+                0, 0, hinst, None)
+        UpdateWindow(self.hwnd)
+        iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
+        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+        try:
+           hicon = LoadImage(hinst, iconPathName, \
+                    win32con.IMAGE_ICON, 0, 0, icon_flags)
+        except:
+          hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+        nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "tooltip")
+        Shell_NotifyIcon(NIM_ADD, nid)
+        Shell_NotifyIcon(NIM_MODIFY, \
+                         (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,\
+                          hicon, "Balloon  tooltip",msg,200,title))
+        # self.show_balloon(title, msg)
+        time.sleep(5)
+        DestroyWindow(self.hwnd)
+        #This unregisters the class so that more messages can be shown
+        classAtom = UnregisterClass(classAtom, hinst)
+    def OnDestroy(self, hwnd, msg, wparam, lparam):
+        nid = (self.hwnd, 0)
+        Shell_NotifyIcon(NIM_DELETE, nid)
+        PostQuitMessage(0) # Terminate the app.
+
+def balloon_tip(title, msg):
+    w=WindowsBalloonTip(title, msg)
+
+if __name__ == '__main__':
+    balloon_tip("Title for popup", "This is the popup's message")
+    balloon_tip("Title 2", "Message 2")
